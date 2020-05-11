9ee138a62101a5dd14aa0f41e217777e37903d0b
Dhiraj Kumar <37096978+dhiraj21kumar@users.noreply.github.com>
2020-05-11 11:38:53 +0530

    first commit

---
+++ Landmarks/LandmarkDetail.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Dhiraj Kumar on 06/05/20.
+//  Copyright © 2020 Dhiraj Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        VStack {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height: 300)
+                .edgesIgnoringSafeArea(.top)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                    
+                HStack {
+                    Text(landmark.park)
+                        .font(.subheadline)
+                    Spacer()
+                    Text(landmark.state)
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .navigationBarTitle(Text(landmark.name), displayMode: .inline)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarkData[0])
+    }
+}
